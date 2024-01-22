0654eb3b0da1a7ba2e0a1d6c05430761ef332151
Jafarscript <lihammedjafar@gmail.com>
2024-01-22 22:31:19 +0100

    Create, Edit, Delete on React

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,65 @@
-// import React from 'react'
+import {useState, useEffect} from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 const DeleteBook = () => {
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBook(response.data.book);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert('An error happened. Please check the console.');
+        console.log(error);
+        setLoading(false);
+      });
+  }, [id]);
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
   return (
-    <div>DeleteBook</div>
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? (
+        <div className="flex justify-center items-center mt-2">
+          <Spinner />
+        </div>
+      ) : null}
+      {book && (
+        <div className="border-2 border-sky-400 rounded-xl p-4">
+          <p>
+            Are you sure you want to delete the book <strong>{book.title}</strong> by {book.author}?
+          </p>
+          <div className="flex mt-4">
+            <button className="p-2 bg-red-600 text-white" onClick={handleDeleteBook}>
+              Delete
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Spinner from "../components/Spinner";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox} from "react-icons/md";
+import BooksTable from "../components/home/BooksTable";
+import BookCard from "../components/home/BookCard";
 
 const Home = () => {
   const [book, setBook] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState('table')
 
   useEffect(() => {
     setLoading(true);
@@ -27,6 +28,10 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg" onClick={() => setShow('table')}>Table</button>
+        <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg" onClick={() => setShow('card')}>Card</button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
@@ -37,54 +42,8 @@ const Home = () => {
         <div className="flex justify-center items-center mt-2">
           <Spinner />
         </div>
-      ) : (
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr>
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">Title</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Author
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Publish Year
-              </th>
-              <th className="border border-slate-600 rounded-md">Operations</th>
-            </tr>
-          </thead>
-          <tbody>
-            {book.map((book, index) => (
-              <tr key={book._id} className="h-8">
-                <td className="border border-slate-700 rounded-md text-center">
-                  {index + 1}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  {book.title}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.author}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.publishedYear}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  <div className="flex justify-center gap-x-4">
-                    <Link to={`/books/details/${book._id}`}>
-                      <BsInfoCircle className="text-2xl text-green-800" />
-                    </Link>
-                    <Link to={`/books/details/${book._id}`}>
-                      <AiOutlineEdit className="text-2xl text-yellow-600" />
-                    </Link>
-                    <Link to={`/books/details/${book._id}`}>
-                      <MdOutlineDelete className="text-2xl text-red-600" />
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+      ) : show === 'table' ? <BooksTable book={book} /> : <BookCard book={book} />
+      }
     </div>
   );
 };
